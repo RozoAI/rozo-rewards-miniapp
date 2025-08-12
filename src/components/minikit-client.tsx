4428d99fdf5f1d955e46d0e7d52730f3b3fa62e2
akbarsaputrait <akbarsaputrait@outlook.com>
2025-08-13 00:23:03 +0700

    feat: initial rainbowkit and check minikit

--- src/components/minikit-client.tsx
+++ src/components/minikit-client.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export function MiniKitClient({ children }: { children: React.ReactNode }) {
   const { setFrameReady, isFrameReady } = useMiniKit();
-
+  console.log("isFrameReady", isFrameReady);
   // The setFrameReady() function is called when your mini-app is ready to be shown
   useEffect(() => {
     if (!isFrameReady) {
