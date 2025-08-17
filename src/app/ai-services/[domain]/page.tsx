15ed7f8fec122184411abefd449cf2ccfa36ea9f
muggledev <shawn@mugglepay.com>
2025-08-18 05:54:54 +0800

    fix style

--- src/app/ai-services/[domain]/page.tsx
+++ src/app/ai-services/[domain]/page.tsx
@@ -299,7 +299,7 @@ export default function AIServiceDetailPage() {
                 rel="noopener noreferrer"
                 className="flex items-center gap-2 text-sm text-blue-600 hover:text-blue-800 transition-colors"
               >
-                <svg class="size-4" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"></path></svg>
+                <svg className="size-4" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"></path></svg>
               </a>
               <a
                 href="https://discord.com/invite/EfWejgTbuU"
