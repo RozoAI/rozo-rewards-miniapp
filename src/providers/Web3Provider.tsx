6dfc724bf6291a720932a0e2a5b10b2bb4cd28f4
akbarsaputrait <akbarsaputrait@outlook.com>
2026-06-23 22:16:49 +0700

    revert: web3provider

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { wagmiAdapter } from "@/lib/appkit";
+import { initializeAppKit, wagmiAdapter } from "@/lib/appkit";
+import { AppKitProvider } from "@reown/appkit/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import { Config, WagmiProvider } from "wagmi";
+import { base } from "viem/chains";
+import { Config, cookieToInitialState, WagmiProvider } from "wagmi";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +16,14 @@ export default function Web3Provider({
   children: React.ReactNode;
   cookies: string | null;
 }) {
-  const initialState = undefined;
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies
+  );
+
+  const [appKitInstance] = useState(() =>
+    typeof window !== "undefined" ? initializeAppKit() : null,
+  );
 
   return (
     <WagmiProvider
@@ -22,7 +31,13 @@ export default function Web3Provider({
       initialState={initialState}
     >
       <QueryClientProvider client={queryClient}>
-        {children}
+        {appKitInstance ? (
+          <AppKitProvider {...appKitInstance} defaultNetwork={base}>
+            {children}
+          </AppKitProvider>
+        ) : (
+          children
+        )}
       </QueryClientProvider>
     </WagmiProvider>
   );
